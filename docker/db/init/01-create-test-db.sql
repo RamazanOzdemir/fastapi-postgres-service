b6199130b4da3d536e79fd4e679e410a50129700
Ramazan Ozdemir <ramazanozdemir2607@gmail.com>
2026-01-06 18:07:29 +0100

    feat: Add test db script

---
+++ docker/db/init/01-create-test-db.sql
@@ -0,0 +1,5 @@
+-- Create new database for testing
+CREATE DATABASE applicant_task_test;
+
+-- Grant all privileges on the new DB to the main PostgreSQL user
+GRANT ALL PRIVILEGES ON DATABASE applicant_task_test TO postgres;
