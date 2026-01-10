ab51b070c45f094a1fba1e735e858b57c5dbc33c
Ramazan Ozdemir <ramazanozdemir2607@gmail.com>
2026-01-10 11:48:40 +0000

    feat: Add routers to main

--- api/app/main.py
+++ api/app/main.py
@@ -1,8 +1,35 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
-app = FastAPI()
+from app import errors
+from app.routers import comment_router, part_router
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World!"}
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    # startup logic (ileride)
+    yield
+    # shutdown logic (ileride)
+
+
+app = FastAPI(
+    title="FastAPI Postgres Service",
+    version="0.1.0",
+    lifespan=lifespan,
+    root_path="/api",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+    allow_origins=["*"],
+)
+
+errors.register_error_handlers(app)
+
+app.include_router(part_router.app_router)
+app.include_router(comment_router.app_router)
